a769099feaa4fb395dc88da4cd5bc827a9ed9f04
Adam Singer <financeCoding@gmail.com>
2014-03-22 19:07:18 -0700

    Automated update

--- lib/taskqueue_v1beta2_api_client.dart
+++ lib/taskqueue_v1beta2_api_client.dart
@@ -11,3 +11,4 @@ export 'package:google_taskqueue_v1beta2_api/src/client_base.dart' show APIReque
 part 'src/client/client.dart';
 part 'src/client/schemas.dart';
 part 'src/client/resources.dart';
+part 'src/client/utils.dart';
